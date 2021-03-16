ce03ef6afbae78c705eb40bc07ab3c3baf760cf8
Federico De Felici <fdf8210@gmail.com>
2021-03-16 18:27:13 +0100

    fixed exception import in commons.h

--- include/Commons.h
+++ include/Commons.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdexcept>
 
 #define DIE_ON_ERROR(VALUE, MSG) if(VALUE == 0){printf(MSG); throw std::runtime_error(MSG);}
 #define DIE_ON_NULL(VALUE, MSG) if(VALUE == NULL){printf(MSG); throw std::runtime_error(MSG);}
